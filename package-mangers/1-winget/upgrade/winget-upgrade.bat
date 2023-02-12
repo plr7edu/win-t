d03ed54db368400044d8b05c1ef2807f0af4d516
plr7edu <104339100+plr7edu@users.noreply.github.com>
2023-02-12 17:44:09 +0530

    Repo-Update: 2023-02-12 17:44:05

---
+++ package-mangers/1-winget/upgrade/winget-upgrade.bat
@@ -0,0 +1,24 @@
+:: Upgrade Application list
+winget upgrade -h --id KeePassXCTeam.KeePassXC 
+winget upgrade -h --id Daum.PotPlayer  
+winget upgrade -h --id qBittorrent.qBittorrent  
+winget upgrade -h --id 7zip.7zip  
+winget upgrade -h --id Obsidian.Obsidian 
+winget upgrade -h --id Logseq.Logseq  
+winget upgrade -h --id GitHub.GitHubDesktop  
+winget upgrade -h --id Nextcloud.NextcloudDesktop  
+winget upgrade -h --id Mega.MEGASync 
+winget upgrade -h --id Google.Drive 
+winget upgrade -h --id Telegram.TelegramDesktop  
+winget upgrade -h --id TeamViewer.TeamViewer  
+winget upgrade -h --id Cryptomator.Cryptomator  
+winget upgrade -h --id Joplin.Joplin 
+winget upgrade -h --id Geany.Geany 
+winget upgrade -h --id BleachBit.BleachBit  
+winget upgrade -h --id Alacritty.Alacritty  
+winget upgrade -h --id VideoLAN.VLC  
+winget upgrade -h --id Spotify.Spotify  
+winget upgrade -h --id Microsoft.PowerToys  
+winget upgrade -h --id WindscribeLimited.Windscribe  
+winget upgrade -h --id GnuPG.GnuPG  
+winget upgrade -h --id gnupg.Gpg4win 
