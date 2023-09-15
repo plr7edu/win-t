b8d295cd448385820b142bd2bd151f0c55447c9b
plr7edu <104339100+plr7edu@users.noreply.github.com>
2023-09-15 15:43:15 +0530

    Repo Update: 2023-09-15 15:43:13

--- scripts/bat/kill-church-presentation.bat
+++ scripts/bat/kill-church-presentation.bat
@@ -8,7 +8,6 @@ taskkill /F /IM AutoHotkeyU64.exe /T > nul
 
 :: Kill EasyWorship7 & EasyWorshop7 Helper
 taskkill /F /IM EasyWorship.exe /T > nul
-taskkill /F /IM EasyWorshipHelper.exe /T > nul
 
 :: Close active virtual desktop
 START %systemdrive%\PLR-WINDOWS-HOST-TOOLS\"Send Windows Key"\"SendWKey v1.1"\SendWKey.exe #{CTRLDOWN}{F4}{CTRLUP}
@@ -16,4 +15,4 @@ START %systemdrive%\PLR-WINDOWS-HOST-TOOLS\"Send Windows Key"\"SendWKey v1.1"\Se
 :: Kill Powerpoint Presentation
 taskkill /F /IM POWERPNT.exe /T > nul
 
-@pause
+::@pause
