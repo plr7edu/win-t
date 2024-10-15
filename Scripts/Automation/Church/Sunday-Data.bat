df8b063b67b8a6f02830d96420ab23e3f908dc46
plr7edu <plr7.edu@outlook.com>
2024-10-15 14:10:03 +0530

    update 2024-10-15-14.10.03

--- Scripts/Automation/Church/Sunday-Data.bat
+++ Scripts/Automation/Church/Sunday-Data.bat
@@ -6,8 +6,8 @@ START C:\"Program Files"\GPSoftware\"Directory Opus"\dopusrt.exe /acmd Go TABGRO
 ::Start New Word File
 START C:\Users\player-1\Documents\Windows-Git-Repos\win-t\Scripts\Automation\Church\New-Word-Document.vbs
 
-:: Start Mixplorer
-Start C:\Users\player-1\AppData\Local\Microsoft\WindowsApps\MicrosoftCorporationII.WindowsSubsystemForAndroid_8wekyb3d8bbwe\WsaClient.exe /launch wsa://com.mixplorer.silver
+:: Start Solid Explorer
+Start C:\Users\player-1\AppData\Local\Microsoft\WindowsApps\MicrosoftCorporationII.WindowsSubsystemForAndroid_8wekyb3d8bbwe\WsaClient.exe /launch wsa://pl.solidexplorer2
 
 ::Start WindowManager with "Sunday-Data" Profile
 WindowManager.exe -MINIMIZE -PROFILE "Sunday-Data"
